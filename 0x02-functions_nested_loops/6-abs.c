47d30ecddc40b4f921c8f5b0b4bea0faaf3203b1
Tshepo Mompati <mompatitshepo@gmail.com>
2023-06-21 14:20:49 -0700

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * abs - Computes the absolute value of an integer
+ * _abs - Computes the absolute value of an integer
+ *@n: integer in question
  *
  * Return: The absolute value of int
  */
