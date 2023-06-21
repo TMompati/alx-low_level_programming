28d0744247985491f9f1d77183812185bc3d7563
Tshepo Mompati <mompatitshepo@gmail.com>
2023-06-21 13:37:11 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,6 +2,7 @@
 
 /**
  * print_last_digit - Prints the last digit of a number to stdout
+ *@n: Number in question
  *
  * Return: The last digit of the number entered
  */
@@ -9,14 +10,15 @@ int print_last_digit(int n)
 {
 	if (n >= 0)
 	{
-		n = n * 1;
 		n = n % 10;
+		_putchar('0' + n);
 		return (n);
 	}
 	else
 	{
 		n = n * -1;
 		n = n % 10;
+		_putchar('0' + n);
 		return (n);
 	}
 }
