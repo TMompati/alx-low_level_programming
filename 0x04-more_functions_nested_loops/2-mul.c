b0d607dcb3de695460f33ba99f527322317ae2ee
Tshepo Mompati <mompatitshepo@gmail.com>
2023-06-22 13:07:10 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ *
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,3 @@
+int _putchar(char c);
+int mul(int a, int b);
+
