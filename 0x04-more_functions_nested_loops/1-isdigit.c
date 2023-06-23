130c6814aaf3e3af17ac3093a3b897cf9c6895c0
Tshepo Mompati <mompatitshepo@gmail.com>
2023-06-23 07:03:20 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for digits 0 through 9
+ * @c: integer to be checked
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	int i;
+
+	for (i =0; i < 10; i++)
+	{
+		if (c == i + '0')
+			return (1);
+	}
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
 int _putchar(char c);
 int mul(int a, int b);
 void print_numbers(void);
+int _isdigit(int c);
+int _isupper(int c);
