20259580b82844727d64900619fa650da3cac6b8
Tshepo Mompati <mompatitshepo@gmail.com>
2023-06-27 07:32:10 -0700

    8 Jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,33 +7,24 @@
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int a, b;
 
-	for (a = 48; a < 51; a++)
+	for (a = 0; a < 24; a++)
 	{
-		for (b = 48; b < 58; b++)
+		for (b = 0; b < 60; b++)
 		{
-			for (c = 48; c < 54; c++)
-			{
-				for (d = 48; d < 58; d++)
-				{
-					_putchar(a);
-					_putchar(b);
-					_putchar(58);
-					_putchar(c);
-					_putchar(d);
-					_putchar(10);
-					if (a == 50 && b == 51 && c == 53 && d == 59)
-						break;
-				}
-				if (a == 50 && b == 51 && c == 53 && d == 59)
-					break;
-			}
-			if (a == 50 && b == 51 && c == 53 && d == 59)
-				break;
+			if (a > 9)
+				_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+
+			_putchar(58);
+
+			if (b > 9)
+				_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+
+			_putchar(10);
 		}
-		if (a == 50 && b == 51 && c == 53 && d == 59)
-			break;
 	}
 }
 
