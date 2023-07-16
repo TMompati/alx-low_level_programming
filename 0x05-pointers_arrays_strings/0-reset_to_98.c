be2d284ef1fe5bd57e6366dfcd0c804eda9540af
Tshepo Mompati <mompatitshepo@gmail.com>
2023-07-16 09:15:10 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value that *n points to, to 98
+ * @n: Pointer parameter to be updated
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
